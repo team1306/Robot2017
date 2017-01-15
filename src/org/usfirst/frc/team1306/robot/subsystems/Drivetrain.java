2cb364c78c64bf0bb9fb2dec6456191621c15e93
JacksonGoth2 <jacksongoth@gmail.com>
2017-01-15 15:47:43 -0600

    Other stuff
    It didn't all sync

--- src/org/usfirst/frc/team1306/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Drivetrain.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team1306.robot.subsystems;
 import org.usfirst.frc.team1306.robot.Constants;
 import org.usfirst.frc.team1306.robot.RobotMap;
 import org.usfirst.frc.team1306.robot.commands.drivetrain.TankDrive;
-
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.TalonControlMode;
 
@@ -21,12 +20,28 @@ public class Drivetrain extends Subsystem {
 	private final CANTalon leftmotor1;
 	private final CANTalon rightmotor1;
 	
+	private static double initialLeft;
+	private static double initialRight;
+	private static double rightCompensation;
+	
 	public Drivetrain() {
 		leftmotor1 = new CANTalon(RobotMap.LEFT_TALON_1_PORT);
-		//leftmotor2 = new CANTalon(RobotMap.LEFT_TALON_2_PORT);
+		leftmotor1.setPosition(0);
 		rightmotor1 = new CANTalon(RobotMap.RIGHT_TALON_1_PORT);
-		//rightmotor2 = new CANTalon(RobotMap.RIGHT_TALON_2_PORT);
+		rightmotor1.setPosition(0);
+		
 		motors = new CANTalon[] {leftmotor1, rightmotor1};
+		
+		/*initialLeft = leftmotor1.getEncPosition();
+		initialRight = rightmotor1.getEncPosition();
+		if(initialLeft > initialRight){
+			rightCompensation = initialRight - initialLeft;
+		} else if(initialRight > initialLeft) {
+			rightCompensation = initialLeft - initialRight;
+		} else {
+			rightCompensation = 0;
+		}*/
+		
 	}
 	
 	/**
@@ -39,10 +54,31 @@ public class Drivetrain extends Subsystem {
 	 */
 	
 	public void tankDrive(double leftVal, double rightVal) {
+		
 		leftmotor1.changeControlMode(TalonControlMode.PercentVbus);
 		rightmotor1.changeControlMode(TalonControlMode.PercentVbus);
-		leftmotor1.set(-leftVal*Constants.SPEED_MODIFIER);
-		rightmotor1.set(rightVal*Constants.SPEED_MODIFIER);
+		
+		//double leftError = Math.abs(leftmotor1.getEncPosition() - (rightmotor1.getEncPosition() + rightCompensation));
+		//double rightError = Math.abs(rightmotor1.getEncPosition() + rightCompensation - leftmotor1.getEncPosition());
+		leftmotor1.set(-leftVal*Constants.SPEED_MODIFIER/**(.5*leftError)*/);
+		rightmotor1.set(rightVal*Constants.SPEED_MODIFIER/**(.5*rightError)*/);
+		
+		SmartDashboard.putNumber("Drivetrain Speed",rightVal*Constants.SPEED_MODIFIER);
+		SmartDashboard.putNumber("Intake Speed",Intake.intakeSpeed);
+		
+		/*
+		 * Currently lowers speed of intake motor when drivetrain speed is above 0.5
+		 */
+		if(rightVal*Constants.SPEED_MODIFIER > .5) {
+			Intake.lowerSpeed();
+		} else {
+			Intake.raiseSpeed();
+		}
+		
+		//SmartDashboard.putNumber("Left Encoder",leftmotor1.getEncPosition());
+		//SmartDashboard.putNumber("Right Encoder",rightmotor1.getEncPosition()/* + rightCompensation*/);
+		//SmartDashboard.putNumber("Left Vel: ",leftmotor1.getEncVelocity());
+		//SmartDashboard.putNumber("Right Vel: ",rightmotor1.getEncVelocity());
 	}
 	
 	public void DrivePID() {
--- src/org/usfirst/frc/team1306/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Intake.java
@@ -1,15 +1,11 @@
 package org.usfirst.frc.team1306.robot.subsystems;
 
-import org.usfirst.frc.team1306.robot.RobotMap;
-import org.usfirst.frc.team1306.robot.commands.spinIntake;
-import org.usfirst.frc.team1306.robot.commands.drivetrain.TankDrive;
+import org.usfirst.frc.team1306.robot.Constants;
 
-import com.ctre.CANTalon;
-import com.ctre.CANTalon.TalonControlMode;
-
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**this subsystem controls hood rollers
  * 
@@ -21,14 +17,49 @@ public class Intake extends Subsystem {
 
 	private final Talon intakeMotor;
 
+	public static double intakeSpeed = Constants.INTAKE_DEFAULT_SPEED;
+	public static double shooterSpeed = Constants.SHOOTER_SPEED;
+	
 	public Intake() {
 		intakeMotor = new Talon(0);
 	}
 	
+	/*
+	 * Method that continually spins intake
+	 * 
+	 */
 	public void SpinIntake() {
-		intakeMotor.set(-.65);
+		//double input = 1.0;
+		//input = SmartDashboard.getNumber("Motor Power: ",input);
+		
+		/*
+		 * For time being shooter speed in positive and intake speed is negative
+		 */
+		intakeMotor.set(shooterSpeed); //Change positive or negative when switching between shooter and intake
 	}
 	
+	/*
+	 * Lowers speed of intake for specific situations
+	 * See tankDrive(); for usage
+	 */
+	public static void lowerSpeed() {
+		
+		intakeSpeed = Constants.INTAKE_DEFAULT_SPEED - Constants.INTAKE_SPEED_CHANGE;
+	}
+	
+	/*
+	 * Raises speed of intake back to Default for specific situations
+	 *  See tankDrive(); for usage
+	 */
+	public static void raiseSpeed() {
+		
+		intakeSpeed = Constants.INTAKE_DEFAULT_SPEED;
+	}
+	
+	/*
+	 * Stops the intake when command is done
+	 * 
+	 */
 	public void stopMotor() {
 		intakeMotor.set(0.0);
 	}
