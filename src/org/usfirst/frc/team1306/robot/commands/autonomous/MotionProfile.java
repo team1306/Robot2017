8a4432f83e9e39ced63a25f871b4ef3dee2923c5
JacksonGoth2 <jacksongoth@gmail.com>
2017-02-12 09:32:16 -0600

    Started pathfinder

--- src/org/usfirst/frc/team1306/robot/commands/autonomous/MotionProfile.java
+++ src/org/usfirst/frc/team1306/robot/commands/autonomous/MotionProfile.java
@@ -2,6 +2,9 @@ package org.usfirst.frc.team1306.robot.commands.autonomous;
 
 import org.usfirst.frc.team1306.robot.Constants;
 import org.usfirst.frc.team1306.robot.commands.CommandBase;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
 
 public class MotionProfile extends CommandBase {
 
@@ -11,9 +14,11 @@ public class MotionProfile extends CommandBase {
 	
 	@Override
 	protected void initialize() {
-		
+		Waypoint[] points = new Waypoint[]	{
+			new Waypoint(0,0,Pathfinder.d2r(-45))
+		};
 	}
-
+		
 	@Override
 	protected void execute() {
 		drivetrain.driveMP(Constants.LEFT_ROTATIONS,Constants.RIGHT_ROTATIONS);
