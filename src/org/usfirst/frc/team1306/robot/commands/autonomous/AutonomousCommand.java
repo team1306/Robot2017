fc9fb69535351f25be273225d7da5d3c3f6a5798
JacksonGoth2 <jacksongoth@gmail.com>
2017-08-06 11:19:57 -0500

    Preperation to test non-90 degree turn profiles

--- src/org/usfirst/frc/team1306/robot/commands/autonomous/AutonomousCommand.java
+++ src/org/usfirst/frc/team1306/robot/commands/autonomous/AutonomousCommand.java
@@ -6,7 +6,6 @@ import org.usfirst.frc.team1306.robot.commands.drivetrain.FollowPath;
 import org.usfirst.frc.team1306.robot.commands.drivetrain.Profile;
 import org.usfirst.frc.team1306.robot.commands.drivetrain.Profile2D;
 import org.usfirst.frc.team1306.robot.commands.geartake.PlaceGear;
-
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -30,22 +29,53 @@ public class AutonomousCommand extends CommandGroup {
 		if(routine.equals(AutoMode.HOPPER)) {
 			
 			ProfileParams params = new ProfileParams(18.25,45,45);
-			addSequential(new Follow2DPath(new Profile2D(params,60,60,90,15))); //Params, DistanceX, DistanceY, Exit Angle, Max Time
+			
+			addParallel(new DeployIntake());
+			if(alliance.equals(Alliance.Red)) {
+				addSequential(new Follow2DPath(new Profile2D(params,80,95.5,90,15))); //Params, DistanceX, DistanceY, Exit Angle, Max Time
+			} else {
+				
+			}
+//			addSequential(new SetSetpoint(Setpoint.AUTO_HOPPER));
+//			addSequential(new SpinShooter(Constants.SHOOT_TIME,Constants.SHOOTER_RPM_SPEED));
 			
 		} else if(routine.equals(AutoMode.LEFT_GEAR)) {
+			
+			ProfileParams params = new ProfileParams(18.25,45,45);
+			
+			if(alliance.equals(Alliance.Red)) {
+				addSequential(new Follow2DPath(new Profile2D(params,99.45,60.16,60,15)));
+			} else {
+				
+			}
+//			addSequential(new PlaceGear());
+//			addSequential(new TimedDrive(0.3,1));
+//			addSequential(new DeployIntake());
 			
 		} else if(routine.equals(AutoMode.MIDDLE_GEAR)) {
 			
 			addSequential(new FollowPath(new Profile(76,20,50,100,4.75),true)); //Distance, Velocity, Accel, Jerk, Max Time
 			addSequential(new PlaceGear());
-			addSequential(new TimedDrive(0.3,1.5));
+			addSequential(new TimedDrive(0.3,1));
 			addSequential(new DeployIntake());
 			
 		} else if(routine.equals(AutoMode.RIGHT_GEAR)) {
 		 	
+			ProfileParams params = new ProfileParams(18.25,45,45);
+			
+			if(alliance.equals(Alliance.Red)) {
+//				addSequential(new Follow2DPath(new Profile2D(params,60,60,60,15)));
+			} else {
+				
+			}
+//			addSequential(new PlaceGear());
+//			addSequential(new TimedDrive(0.3,1));
+//			addSequential(new DeployIntake());
+			
 		} else if(routine.equals(AutoMode.BASELINE)) {
 			
-			addSequential(new FollowPath(new Profile(100,18.25,45,45,15),true));
+			addSequential(new FollowPath(new Profile(70,18.25,45,45,15),true));
+			addSequential(new DeployIntake());
 		}
 	}
 }
