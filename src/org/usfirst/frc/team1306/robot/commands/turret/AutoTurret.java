cfda129adbd3ce0bf3e22fdfec5efce0bcee88af
JacksonGoth2 <jacksongoth@gmail.com>
2017-09-12 20:01:10 -0500

    Vision adjustments - tracking works

--- src/org/usfirst/frc/team1306/robot/commands/turret/AutoTurret.java
+++ src/org/usfirst/frc/team1306/robot/commands/turret/AutoTurret.java
@@ -41,7 +41,7 @@ public class AutoTurret extends CommandBase {
 		
 		if(vision.seeTarget()) {
 			
-			if(yawList.size() < 20) { //Fills up initial array
+			if(yawList.size() < 10) { //Fills up initial array
 				yawList.add(vision.getYaw());
 			} else {
 				yawList.remove(0); //Removes oldest data from list
@@ -54,11 +54,11 @@ public class AutoTurret extends CommandBase {
 			}
 			averagedYaw = accumulator / yawList.size();
 			
-			if(Math.abs(averagedYaw) > 4) {
+			if(Math.abs(averagedYaw) > 4.5) {
 				if(averagedYaw > 0) {
-					visionRotAdj = ((averagedYaw / 15)/10) + 0.1;
+					visionRotAdj = ((averagedYaw / 15)/12) + 0.075;
 				} else {
-					visionRotAdj = ((averagedYaw / 15)/10) - 0.1;
+					visionRotAdj = ((averagedYaw / 15)/12) - 0.075;
 				}
 			} else {
 				visionRotAdj = 0;
@@ -68,45 +68,6 @@ public class AutoTurret extends CommandBase {
 			SmartDashboard.putNumber("visionRotAdj",visionRotAdj);
 			
 			turret.setSpeed(visionRotAdj);
-			
-//			double percentAdj = 0;
-//			
-//			if(Math.abs(averagedYaw) > 4) {
-//				if(averagedYaw > 0) {
-//					percentAdj = 20;
-//				} else {
-//					percentAdj = -20;
-//				}
-//				visionRotAdj = ((((averagedYaw/15)*40) + percentAdj)/100)*218;
-//			} else {
-//				visionRotAdj = 0;
-//			}
-//			
-//			SmartDashboard.putNumber("AveragedYaw",averagedYaw);
-//			SmartDashboard.putNumber("PercentAdj",percentAdj);
-//			SmartDashboard.putNumber("visionRotAdj",visionRotAdj/85);
-//			
-//			if(visionRotAdj > 60) {
-//				visionRotAdj = 60;
-//			} else if(visionRotAdj < -60) {
-//				visionRotAdj = -60;
-//			}
-//			
-//			turret.setRPM(visionRotAdj/60);
-			
-//			visionRotAdj = (averagedYaw/360) * Constants.TURRET_GEAR_CONVERSION;
-//			double leftLimit = Constants.TURRET_LEFT_ROT_LIMIT, rightLimit = Constants.TURRET_RIGHT_ROT_LIMIT;
-//			double currentRot = turret.getPosition();
-//			
-//			SmartDashboard.putNumber("visionRotAdj",visionRotAdj);
-//			
-//			if(visionRotAdj + currentRot < rightLimit && visionRotAdj + currentRot > leftLimit) {
-//				if(recoveryTimer.hasPeriodPassed(0.5)) {
-//					turret.moveRot(visionRotAdj + currentRot);
-//					recoveryTimer.reset();
-//					recoveryTimer.start();
-//				}
-//			}
 			
 		} else if(manualVal > Constants.DEADBAND || manualVal < -Constants.DEADBAND) {
 			turret.setSpeed(OI.getJoyVal(Controller.S, Joystick.L, Axis.X) * -0.2);
